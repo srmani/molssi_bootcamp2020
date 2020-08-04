eff098bd4b3a124a9558400d85253661afb04c72
Sriramvignesh Mani <m.sriramvignesh@gmail.com>
2020-08-04 12:29:09 -0500

    added docstrings to functions.py

--- molecool/functions.py
+++ molecool/functions.py
@@ -9,6 +9,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def calculate_distance(rA, rB):
+	"""
+	Calculate the distance between two points
+	
+	Parameters
+	----------
+	rA, rB : np.ndarray
+		The coordinates of each point
+	
+	Returns
+	-------
+	dist : float
+		The distance between the two points.
+		
+	Examples
+	--------
+	>>> r1 = np.array([0,0,0])
+	>>> r2 = np.array([1,0,0])
+	>>> calculate_distance(r1, r2)
+	1
+	"""
     # This function calculates the distance between two points given as numpy arrays.
     d=(rA-rB)
     dist=np.linalg.norm(d)
