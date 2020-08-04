f96c8d3aebc670d60dce8b31da29c9cbbef9badf
Sriramvignesh Mani <m.sriramvignesh@gmail.com>
2020-08-04 12:16:47 -0500

    improved readability of open_pdb function in functions.py

--- molecool/functions.py
+++ molecool/functions.py
@@ -18,14 +18,14 @@ def open_pdb(f_loc):
     # This function reads in a pdb file and returns the atom names and coordinates.
     with open(f_loc) as f:
         data = f.readlines()
-    c = []
+    coordinates = []
     sym = []
-    for l in data:
-        if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
-            sym.append(l[76:79].strip())
-            c2 = [float(x) for x in l[30:55].split()]
-            c.append(c2)
-    coords = np.array(c)
+    for line in data:
+        if 'ATOM' in line[0:6] or 'HETATM' in line[0:6]:
+            sym.append(line[76:79].strip())
+            c2 = [float(x) for x in line[30:55].split()]
+            coordinates.append(c2)
+    coords = np.array(coordinates)
     return sym, coords
 
 atomic_weights = {
